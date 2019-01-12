55cca6078a902dbe3fb02f98ef34724fd0beb3a4
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-12 09:32:22 -0500

    Interim checkin

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/" do 
+  @user_input = params[:user_input]
+    erb :user_input
+  end
+
+  post "/user_input" do
+    @piglatiner = PigLatinizer.new(params[:user_input])
+    "#{@piglatiner}"
+    erb :user_input
+  end
+  
 end
