10d06a6f2b6907453fefdb1e7b665590eb72f0be
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-13 20:45:53 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,14 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get "/" do 
-  @user_input = params[:user_input]
-    erb :user_input
+  erb :user_input
   end
 
-  post "/user_input" do
-    @piglatiner = PigLatinizer.new(params[:user_input])
-    "#{@piglatiner}"
-    erb :user_input
+  post "/piglatinize" do
+    @piglatinizer = PigLatinizer.new
+    @translated = @piglatinizer.piglatinize(params[:user_phrase])
+    erb :piglatinize
   end
-  
+
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,26 +1,24 @@
 
-class Piglatinizer < Sinatra::Base
-attr_reader :word, :string
+class PigLatinizer 
 
-def piglatinize(word)
-  non_pig_latin_words = ["i","me", "to", "too", "a", "an", "in", "and", "on"]
-  vowels = ["a", "e", "i", "o", "u"]
-  
-  if non_pig_latin_words.include?(word)
-    word
-  elsif vowels.include? word[0]
-    word << "ay"
-  else
-    consonants = ""
-    while !vowel.include?(word[0])
-      consonants << word[0]
-      word = word.split("")[1..-1].join
+
+def piglatinize(words)
+ 
+  vowels = ["a", "A", "e", "E", "I", "i", "o","O", "U", "u"]
+  pig_words = words.split(' ').map do |word|
+    if(vowels.include? word[0])
+      word + 'way'
+    else
+      parts = word.split(/([aeiou].*)/)
+
+      if parts.length > 1
+        parts[1] + parts[0] + 'ay'
+      else
+        word
+      end  
     end
-    word + consonants + 'ay'
+    end
+    pig_words.join(' ')
   end
-  end
-
-  def to_pig_latin(string)
-    string.split.collect {|word| piglatinize(word)}.join("")
-  end
+  
 end
